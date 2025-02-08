18313542398f8e68d5cd020de511b2cd7b639fcd
mapirelli <mapirelli@gmail.com>
2025-02-08 17:39:58 +0000

    Deploying to gh-pages from @ mapirelli/blog@e16011a87483a125e57d455d488f40f502288a94 🚀

---
+++ _nuxt/DhJMCIpP.js
@@ -0,0 +1,1 @@
+import{d as o,c as r,i as a,P as s,a4 as l,j as n}from"./BywP7pBw.js";const c=o({__name:"Alert",props:{color:{type:String,default:"orange"}},setup(e){return(t,d)=>(a(),r("div",{class:"alert",style:l({"border-color":e.color})},[s(t.$slots,"default",{},void 0,!0)],4))}}),p=Object.assign(n(c,[["__scopeId","data-v-46dbbe98"]]),{__name:"Alert"});export{p as default};
