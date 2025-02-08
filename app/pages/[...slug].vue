91963ee07542d331b20dd386f391d667484e15fe
Mariano Pirelli <mapirelli@gmail.com>
2025-02-08 16:09:12 +0100

    chore: route bug fix

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
-const route = useRoute()
+const route = useRoute();
 
-const { data: page } = await useAsyncData('page-' + route.path, () => {
-  return queryCollection('content').path(route.path).first()
-})
+// remove trailing slash from path
+const actualPath = route.path.replace(/\/$/, "");
+
+const { data: page } = await useAsyncData("page-" + actualPath, () => {
+  return queryCollection("content").path(actualPath).first();
+});
 
 if (!page.value) {
-  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Page not found",
+    fatal: true,
+  });
 }
 </script>
 
 <template>
-  <ContentRenderer
-    v-if="page"
-    :value="page"
-  />
+  <ContentRenderer v-if="page" :value="page" />
 </template>
