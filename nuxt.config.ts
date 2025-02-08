01e35d76ed669fb43ecf04a57083988f35b52fe3
Mariano Pirelli <mapirelli@gmail.com>
2025-02-08 16:02:30 +0100

    chore: new dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content"],
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
   content: {
     preview: {
       api: "https://api.nuxt.studio",
+    },
+  },
+  router: {
+    options: {
+      strict: true,
     },
   },
   devtools: { enabled: true },
